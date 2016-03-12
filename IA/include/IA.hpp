10380adebb383c319723dfec3963a6d1b876b720
Clement Fontenaille <c.fontenaille@gmail.com>
2016-03-12 13:14:32 +0100

    azeaze

--- IA/include/IA.hpp
+++ IA/include/IA.hpp
@@ -6,16 +6,5 @@
 using namespace std;
 using namespace ainet16;
 
-//~ static Session session;
-//~ static Welcome welcome;
-//~ static GameParameters p;
-//~ static string address;
-//~ static string port_str;
-//~ static int port;
-//~ static string name;
-//~ static Actions actions;
-
-
-
 void play_turn(Actions & actions, GameParameters &p, Session & session);
 float dist(float x1, float x2, float y1, float y2);
