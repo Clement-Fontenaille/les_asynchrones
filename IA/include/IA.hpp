00aa9fbe6a494cc89bddf6eb3f9150fbc70b2202
Clement Fontenaille <c.fontenaille@gmail.com>
2016-03-12 16:37:50 +0100

    aze

--- IA/include/IA.hpp
+++ IA/include/IA.hpp
@@ -22,41 +22,17 @@ extern set<int> played_cells;
 void play_turn();
 
 float dist(float x1, float x2, float y1, float y2);
-float cellDistance(const TurnPlayerCell & C1, const TurnPlayerCell & C2);
+float dist(const TurnPlayerCell & C1, const TurnPlayerCell & C2);
+float dist(const Position & p1,const Position & p2);
 float radius(const TurnPlayerCell & C);
-
+float radius(const NeutralCell & C);
+float radius(const TurnVirus & C);
 float moveDistance(const TurnPlayerCell & cell);
 bool dangerEnnemyCell(const TurnPlayerCell & ennemyCell, const TurnPlayerCell & myCell);
 bool canEatEnnemyCell(const TurnPlayerCell & ennemyCell, const TurnPlayerCell & myCell);
+bool canEatNeutralCell(const TurnPlayerCell & ennemyCell, const NeutralCell & target);
 vector<int> isInDangerFromAnotherCell(const TurnPlayerCell & myCell);
-
 int getNearestDangerousCell(vector<int> dangerousCells, const TurnPlayerCell & myCell);
+vector<NeutralCell> get_edible_neutrals_in_range(const TurnPlayerCell & myCell);
 void goToOppositeDirectionFromDangerousCell(int ennemiIndex, const TurnPlayerCell & myCell);
-
-//~ struct Position
-//~ {
-	//~ float x; //! L'abscisse, comprise dans [0,map_width[
-	//~ float y; //! L'ordonnée, comprise dans [0,map_height[
-//~ };
-//~ 
-//~ struct MoveAction
-//~ {
-	//~ int pcell_id;
-	//~ Position position;
-//~ };
-//~ 
-//~ struct DivideAction
-//~ {
-	//~ int pcell_id;
-	//~ Position position;
-	//~ float mass;
-//~ };
-//~ 
-//~ struct CreateVirusAction
-//~ {
-	//~ int pcell_id;
-	//~ Position position;
-//~ };
-
-
-
+Position select_safe_move(const TurnPlayerCell arg, const Position & target);
--- IA/src/IA.cpp
+++ IA/src/IA.cpp
@@ -10,30 +10,40 @@ GameParameters * p;
 Session * session;
 Welcome * welcome;
 set<int> played_cells;
+set<int> targeted_neutral_cells;
 
 
 void play_turn(){
 	played_cells.clear();
+	targeted_neutral_cells.clear();
 	
 	/** strategie de fuite **/
 	
 	// pour chacune de mes cellules
 	for (const TurnPlayerCell & cell : session->my_player_cells())
 	{
+		if (*(targeted_neutral_cells.lower_bound(cell.pcell_id))==cell.pcell_id) continue;
 		// recupere l'iD de la cellule ennemie la + dangereuse
 		vector<int> dangerousCells = isInDangerFromAnotherCell(cell);
 		if (dangerousCells.size())
 		{
 			int ennemiIndex = getNearestDangerousCell(dangerousCells, cell);
 			goToOppositeDirectionFromDangerousCell(ennemiIndex, cell);
+			played_cells.insert(cell.pcell_id);
+			continue;
 		}
-		else
-		{
-			// avance Random
-			actions->add_move_action(cell.pcell_id, float(rand())/RAND_MAX*p->map_width, float(rand())/RAND_MAX*p->map_height);
+		vector<NeutralCell> edible = get_edible_neutrals_in_range(cell);
+		if (edible.size() > 0){
+			actions->add_move_action(cell.pcell_id, edible[0].position.x, edible[0].position.y);
+			played_cells.insert(cell.pcell_id);
+			targeted_neutral_cells.insert(edible[0].id);
+		}
 
-		}
+		// DEFAULT : avance Random
+		actions->add_move_action(cell.pcell_id, float(rand())/RAND_MAX*p->map_width, float(rand())/RAND_MAX*p->map_height);
+		played_cells.insert(cell.pcell_id);
 	}
+		
 
 	
 	
@@ -62,15 +72,31 @@ float dist(float x1, float x2, float y1, float y2)
 }
 
 // distance entre 2 cellules
-float cellDistance(const TurnPlayerCell & C1, const TurnPlayerCell & C2)
+float dist(const TurnPlayerCell & C1, const TurnPlayerCell & C2)
 {
 	return dist( C1.position.x, C2.position.x, C1.position.y, C2.position.y);
+}
+
+// distance entre 2 positions
+float dist(const Position & p1,const Position & p2)
+{
+	return dist( p1.x, p2.x, p1.y, p2.y);
 }
 
 // get radius
 float radius(const TurnPlayerCell & C)
 {
 	return C.mass*p->radius_factor;
+}
+// get radius
+float radius(const NeutralCell & C)
+{
+	return C.mass*p->radius_factor;
+}
+// get radius
+float radius(const TurnVirus & C)
+{
+	return p->virus_mass*p->radius_factor;
 }
 
 // distance qu'une cellule peut franchir en 1 tour
@@ -84,7 +110,7 @@ bool dangerEnnemyCell(const TurnPlayerCell & ennemyCell, const TurnPlayerCell & 
 {
 	if(
 		(radius(myCell) < radius(ennemyCell)) &&
-		(cellDistance(ennemyCell, myCell) < (moveDistance(ennemyCell) + radius(ennemyCell)))
+		(dist(ennemyCell, myCell) < (moveDistance(ennemyCell) + radius(ennemyCell)))
 	) 
         return true;
     else
@@ -96,7 +122,7 @@ bool canEatEnnemyCell(const TurnPlayerCell & ennemyCell, const TurnPlayerCell & 
 {
 	if(
 		(radius(myCell) > radius(ennemyCell)) &&
-		(cellDistance(ennemyCell, myCell) < (moveDistance(myCell) + radius(myCell)))
+		(dist(ennemyCell, myCell) < (moveDistance(myCell) + radius(myCell)))
 	) 
         return true;
     else
@@ -124,17 +150,31 @@ vector<int> isInDangerFromAnotherCell(const TurnPlayerCell & myCell)
 // recupere l'index du celle ennemi le + dangereux
 int getNearestDangerousCell(vector<int> dangerousCells, const TurnPlayerCell & myCell)
 {
-	double minDist = cellDistance(session->ennemy_player_cells()[0], myCell);
+	double minDist = dist(session->ennemy_player_cells()[0], myCell);
 	int index = 1;
 	for (int i=1; i<dangerousCells.size(); i++)
 	{
-		if (cellDistance(session->ennemy_player_cells()[i], myCell) < minDist)
+		if (dist(session->ennemy_player_cells()[i], myCell) < minDist)
 		{
-			minDist = cellDistance(session->ennemy_player_cells()[i], myCell);
+			minDist = dist(session->ennemy_player_cells()[i], myCell);
 			index = i;
 		}
 	}
 	return index;
+}
+
+vector<NeutralCell> get_edible_neutrals_in_range(const TurnPlayerCell & myCell){
+	vector<NeutralCell> r; 
+	for (const NeutralCell & cell : session->neutral_cells())
+	{
+		// ignore already targeted neutral cell 
+		if (*(targeted_neutral_cells.lower_bound(cell.id))==cell.id) continue;
+		// ignore if out of range
+		if (dist(myCell.position, cell.position) > max(radius(myCell),radius(cell))) continue;
+		//else
+		r.push_back(cell);
+	}
+	return r;
 }
 
 // va dans la direction opposée
@@ -143,6 +183,10 @@ void goToOppositeDirectionFromDangerousCell(int ennemiIndex, const TurnPlayerCel
 	// Action 1: let us try to move cell (id=73) to position (x=42, y=4242)
 	actions->add_move_action(myCell.pcell_id, session->ennemy_player_cells()[ennemiIndex].position.x*(-1), 
 									   session->ennemy_player_cells()[ennemiIndex].position.y*(-1));
+}
+
+Position select_safe_move(const TurnPlayerCell arg, const Position & target){
+	return target;
 }
 
 
