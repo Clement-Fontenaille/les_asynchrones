3947311161c768a02588da98b4bab4056e5652dc
Nathalie Möller <nathalie.moller@prism.uvsq.fr>
2016-03-12 13:43:04 +0100

    petites fonctions

--- IA/include/IA.hpp
+++ IA/include/IA.hpp
@@ -9,7 +9,12 @@ using namespace ainet16;
 static Actions actions;
 static GameParameters p;
 
+float max(float a, float b);
+
 void play_turn(Actions & actions, GameParameters &p, Session & session);
 float dist(float x1, float x2, float y1, float y2);
+float cellDistance(const TurnPlayerCell & C1, const TurnPlayerCell & C2);
+float radius(const TurnPlayerCell & C);
 
-
+float moveDistance(const TurnPlayerCell & cell);
+bool dangerEnnemyCell(const TurnPlayerCell & ennemyCell, const TurnPlayerCell & myCell);
